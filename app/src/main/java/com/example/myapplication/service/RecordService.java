aa567a9df04e58bde951b8bbbc7a79925214702a
MQY1005 <2430792388@qq.com>
2019-10-23 11:37:00 +0800

    播放记录

---
+++ app/src/main/java/com/example/myapplication/service/RecordService.java
@@ -0,0 +1,15 @@
+package com.example.myapplication.service;
+
+import com.example.myapplication.entity.Records;
+
+import java.util.List;
+
+
+
+public interface RecordService {
+    List<Records> get(String username);
+    void save(Records records);
+    void modify(Records records);
+    void remove(Records records);
+    void removeAll(Records records);
+}
